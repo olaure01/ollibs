aa08baccff60e75291a12c053d5bdf6854e95908
Christophe LUCAS <kirsuh.cl@gmail.com>
2019-10-25 15:10:47 +0200

    additional lemmas on nth

--- List_Type_more.v
+++ List_Type_more.v
@@ -424,6 +424,26 @@ Proof with try assumption; try reflexivity.
     right...
 Qed.
 
+Lemma nth_In_Type {A} : forall n l (d:A), n < length l ->
+  In_Type (nth n l d) l.
+Proof.
+  unfold lt; induction n as [| n hn]; simpl; destruct l; simpl; intros d ie;
+    try (now inversion ie).
+  - left; reflexivity.
+  - right; apply hn; auto with arith.
+Qed.
+
+Lemma In_nth_Type {A} l (x:A) d : In_Type x l ->
+  { n : _ & n < length l & nth n l d = x }.
+Proof.
+  induction l as [|a l IH]; intros Hin.
+  - inversion Hin.
+  - destruct Hin as [Hin|Hin].
+    + subst; exists 0; simpl; auto with arith.
+    + destruct (IH Hin) as [n Hn Hn'].
+      exists (S n); simpl; auto with arith.
+Qed.
+
 
 (** ** Set inclusion on list *)
 
