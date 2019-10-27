e40c0dff7c71cc1b2567e1a7f691cd54a6db79d1
Olivier Laurent <olivier.laurent@ens-lyon.fr>
2019-10-27 13:04:41 +0100

    use notation {'(_,_) ... }

--- List_Type_more.v
+++ List_Type_more.v
@@ -198,7 +198,7 @@ Ltac dichot_Type_elt_app_exec H :=
 Lemma trichot_Type_elt_app {A} : forall l1 (a : A) l2 l3 l4 l5,
   l1 ++ a :: l2 = l3 ++ l4 ++ l5 ->
      { l2' | l1 ++ a :: l2' = l3 /\ l2 = l2' ++ l4 ++ l5 }
-   + { pl | l1 = l3 ++ fst pl /\ fst pl ++ a :: snd pl = l4 /\ l2 = snd pl ++ l5 }
+   + {'(l3', l4') | l1 = l3 ++ l3' /\ l3' ++ a :: l4' = l4 /\ l2 = l4' ++ l5 }
    + { l5' | l1 = l3 ++ l4 ++ l5' /\ l5' ++ a :: l2 = l5 }.
 Proof with try reflexivity ; try assumption.
 induction l1 ; induction l3 ; intros ;
@@ -276,7 +276,7 @@ Ltac trichot_Type_elt_elt_exec H :=
 
 Lemma app_eq_map_Type {A B} : forall (f : A -> B) l1 l2 l3,
   l1 ++ l2 = map f l3 ->
-    { pl | l3 = fst pl ++ snd pl /\ l1 = map f (fst pl) /\ l2 = map f (snd pl) }.
+    {'(l1', l2') | l3 = l1' ++ l2' /\ l1 = map f l1' /\ l2 = map f l2' }.
 Proof with try assumption ; try reflexivity.
 intros f.
 induction l1 ; intros.
@@ -291,7 +291,7 @@ Qed.
 
 Lemma cons_eq_map_Type {A B} : forall (f : A -> B) a l2 l3,
   a :: l2 = map f l3 ->
-    { pl | l3 = fst pl :: snd pl /\ a = f (fst pl) /\ l2 = map f (snd pl) }.
+    {'(a', l2') | l3 = a' :: l2' /\ a = f a' /\ l2 = map f l2' }.
 Proof.
 intros f a l2 l3 H.
 destruct l3 ; inversion H ; subst.
@@ -368,7 +368,7 @@ Proof with try assumption.
   - simpl in F2R.
     destruct Forall2_Type_app_inv_l with A B R a (concat L) l...
     destruct x.
-    destruct x.
+    destruct y.
     simpl in *.
     destruct IHL with l1 R as [L' p1 p2]...
     split with (l0 :: L').
@@ -633,3 +633,4 @@ Section In_Forall_Type.
   Qed.
 
 End In_Forall_Type.
+
