596929a031fe6a84df5cc0e5b1766645947e8788
Olivier Laurent <olivier.laurent@ens-lyon.fr>
2022-04-06 15:06:31 +0200

    add option_test

--- funtheory.v
+++ funtheory.v
@@ -15,6 +15,10 @@ end.
 
 Definition option_map A B (f : A -> B) := option_eval_default None (fun x => Some (f x)).
 
+Definition option_test A := @option_eval_default A Prop True.
+
+Definition option_testT A := @option_eval_default A Type unit.
+
 
 (** * Retraction pairs *)
 
